c1e9e1cb3a0022f45bf9d2774cdff2b561a10674
Andrey <smail2204973@gmail.com>
2024-02-18 10:47:00 +0300

    Pr1 is Done

--- src/Coffee.kt
+++ src/Coffee.kt
@@ -1,2 +1,6 @@
-enum class Coffee {
+enum class Coffee (var water: Int, var milk: Int, var beans: Int) {
+    Espresso (60, 0, 10),
+    Americano (120, 0, 10),
+    Cappuccino(120, 60, 20),
+    Latte(240, 120, 20)
 }
--- src/CoffeeMachine.kt
+++ src/CoffeeMachine.kt
@@ -1,2 +1,79 @@
-class main {
+class CoffeeMachine() {
+    var water: Int = 2000
+    var milk: Int = 1000
+    var beans: Int = 500
+
+    private fun checkCoffee(name: String, coffee: Coffee) {
+        if (coffee.water > water) {
+            println("Недостаточно воды!\n")
+        } else if (coffee.milk > milk) {
+            println("Недостаточно молока!\n")
+        } else if (coffee.beans > beans) {
+            println("Недостаточно кофе!\n")
+        } else {
+            water -= coffee.water
+            milk -= coffee.milk
+            beans -= coffee.beans
+            println("$name готов\n")
+        }
+    }
+
+    fun start() {
+        println("Кофемашина готова к работе")
+        while (true) {
+            println("Введите команду\n>>")
+            var input = readlnOrNull()
+            when (input) {
+                "выключить" -> {
+                    print("До свидания!\n")
+                    break
+                }
+
+                "наполнить" -> {
+                    water = 2000
+                    milk = 1000
+                    beans = 500
+                    print("Ингридиенты пополнены\n")
+                }
+
+                "статус" -> print("Ингридиентов осталось:\n$water мл воды\n$milk мл молока\n$beans гр кофе\n")
+                "кофе" -> {
+                    print("Введите название напитка или \"назад\" для возврата в главное меню\n>>")
+                    input = readlnOrNull()
+                    when (input) {
+                        "назад" -> {}
+                        "эспрессо" -> {
+                            checkCoffee(input, Coffee.Espresso)
+                        }
+
+                        "американо" -> {
+                            checkCoffee(input, Coffee.Americano)
+                        }
+
+                        "капучино" -> {
+                            checkCoffee(input, Coffee.Cappuccino)
+                        }
+
+                        "латте" -> {
+                            checkCoffee(input, Coffee.Latte)
+                        }
+
+                        else -> {
+                            print("Рецепт не найден!\n")
+                        }
+                    }
+                }
+
+                else -> {
+                    print("Неизвестная команда\n")
+                }
+            }
+        }
+    }
+}
+
+
+fun main() {
+    val coffeeMachine = CoffeeMachine()
+    coffeeMachine.start()
 }
